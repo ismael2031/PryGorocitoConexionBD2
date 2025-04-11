5708a665a7fa3075b1a773bd54aef1458d53cb66
Alumno <Alumno@PC68>
2025-04-11 13:45:59 -0300

    Agregar archivos de proyecto.

---
+++ ClsConexionBD.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PryGorocitoConexionBD2
+{
+    internal class ClsConexionBD
+    {
+        //cadena de conexion
+        string cadenaConexion = "Server=localhost;Database=Ventas2;Trusted_Connection=True;";
+
+        //conector
+        private SqlConnection conexionBaseDatos;
+
+        //comando
+        SqlCommand comandoBaseDatos;
+
+        public string nombreBaseDeDatos;
+
+        // método para abrir la conexión
+        public void ConectarBD()
+        {
+            try
+            {
+                conexionBaseDatos = new SqlConnection(cadenaConexion);
+                conexionBaseDatos.Open();
+                nombreBaseDeDatos = conexionBaseDatos.Database;
+
+                MessageBox.Show("Conexión exitosa a la base de datos: " + nombreBaseDeDatos);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error al conectar: " + error.Message);
+            }
+        }
+
+        // método para obtener la conexión abierta
+        public SqlConnection ObtenerConexion()
+        {
+            return conexionBaseDatos;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PryGorocitoConexionBD2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCargarcontactos = new System.Windows.Forms.Button();
+            this.dgvContactos = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnCargarcontactos
+            // 
+            this.BtnCargarcontactos.Location = new System.Drawing.Point(307, 315);
+            this.BtnCargarcontactos.Name = "BtnCargarcontactos";
+            this.BtnCargarcontactos.Size = new System.Drawing.Size(128, 35);
+            this.BtnCargarcontactos.TabIndex = 1;
+            this.BtnCargarcontactos.Text = "Contactos";
+            this.BtnCargarcontactos.UseVisualStyleBackColor = true;
+            this.BtnCargarcontactos.Click += new System.EventHandler(this.BtnCargarcontactos_Click);
+            // 
+            // dgvContactos
+            // 
+            this.dgvContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContactos.Location = new System.Drawing.Point(6, 19);
+            this.dgvContactos.Name = "dgvContactos";
+            this.dgvContactos.Size = new System.Drawing.Size(708, 275);
+            this.dgvContactos.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnCargarcontactos);
+            this.groupBox1.Controls.Add(this.dgvContactos);
+            this.groupBox1.Location = new System.Drawing.Point(40, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(720, 375);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCargarcontactos;
+        private System.Windows.Forms.DataGridView dgvContactos;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
